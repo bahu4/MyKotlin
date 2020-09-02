f6d665fda89d332f61c786380e2b691115c3abcd
bahu4 <bahu4@mail.ru>
2020-09-02 23:28:06 +0300

    hw2

---
+++ app/src/main/java/com/example/mykotlin/data/entity/Data.kt
@@ -0,0 +1,3 @@
+package com.example.mykotlin.data.entity
+
+data class Data(val title: String, val task: String, val noteColor: Int)
---
+++ app/src/main/java/com/example/mykotlin/ui/main/MainViewState.kt
@@ -0,0 +1,6 @@
+package com.example.mykotlin.ui.main
+
+import com.example.mykotlin.data.entity.Data
+
+data class MainViewState(val taskList: List<Data>) {
+}
